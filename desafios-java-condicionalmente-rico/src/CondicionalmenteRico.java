33655f06e00e48920ffeb6fe6b5868ee4421afa4
Izael silva <izaell.oficial@gmail.com>
2023-10-23 18:09:33 -0300

    Update CondicionalmenteRico.java

--- desafios-java-condicionalmente-rico/src/CondicionalmenteRico.java
+++ desafios-java-condicionalmente-rico/src/CondicionalmenteRico.java
@@ -50,8 +50,7 @@
 import java.util.Scanner;
 
 public class CondicionalmenteRico {
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args) {
         // Entrada de dados
         Scanner scanner = new Scanner(System.in);
 
