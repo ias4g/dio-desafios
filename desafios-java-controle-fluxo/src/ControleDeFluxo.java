6f8ca8b023f48deacc0e16037ed5d9b2e82ca9d7
Izael silva <izaell.oficial@gmail.com>
2023-10-29 14:06:51 -0300

    Create ControleDeFluxo.java

---
+++ desafios-java-controle-fluxo/src/ControleDeFluxo.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class ControleDeFluxo {
+    public static void main(String[] args) {
+
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+
+        System.out.print("Digite o primeiro parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        terminal.close();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+
+    private static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 1; i <= contagem; i++) {
+            if (i < 10) {
+                System.out.println("Imprimindo o número 0" + i);
+            } else {
+                System.out.println("Imprimindo o número " + i);
+            }
+        }
+
+    }
+}
