26c6eab8f8787dc04f20a97debe360dc3ebacbe7
Izael silva <izaell.oficial@gmail.com>
2023-11-09 15:46:44 -0300

    Update challenge.js

--- desafios-de-codigo-pontuando-experiencias-xp/challenge.js
+++ desafios-de-codigo-pontuando-experiencias-xp/challenge.js
@@ -2,15 +2,15 @@
 //- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
 //- "print": imprime um texto de saída (output), pulando linha.
 
-// Para converter os valores inseridos na entrada, utilizamos a função JavaScript parseInt() que converte a parte inicial da string em um número inteiro.
+// Entrada de dados. Lembre-se: O parseInt(()) é importante para a conversão dos valores de entrada(String) para um valor numérico(int).
 
 const { gets, print } = require('./functionsAux')
 
-const posicaoInicial = parseInt(gets())
-const totalPassos = parseInt(gets())
+let num1 = parseInt(gets());
+let num2 = parseInt(gets());
 
-//TODO: Calcule a posição final do herói:
-const posicaoFinal = posicaoInicial + totalPassos;
+//TODO: Implemente a lógica para exibir o xpGanho:
+const xpGanho = num1 * num2 * 100;
 
-// Imprime a posição final
-print("Posicao final do heroi:" + posicaoFinal);
+// Imprime a quantidade de XP ganho
+print("Voce ganhou " + xpGanho + " XP!");
