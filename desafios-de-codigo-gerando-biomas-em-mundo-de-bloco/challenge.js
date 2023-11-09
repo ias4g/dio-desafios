1364da3c392b11478b28ffadfb77559ac1ea4a57
Izael silva <izaell.oficial@gmail.com>
2023-11-09 17:34:14 -0300

    Update challenge.js

--- desafios-de-codigo-gerando-biomas-em-mundo-de-bloco/challenge.js
+++ desafios-de-codigo-gerando-biomas-em-mundo-de-bloco/challenge.js
@@ -9,14 +9,16 @@ const { gets, print } = require('./functionsAux')
 // O parseInt(()) vai converter os valores de entrada(string) para um valor numérico(Int).
 const quantidadeGolpes = parseInt(gets());
 
+print(quantidadeGolpes);
+
 // TODO: Defina aqui os tipos de minerais Carvao, Ferro, Diamante e Pedra
-let minerais = ["   ", " ", "   ", "   "];
+let minerais = ["Carvao", "Ferro", "Diamante", "Pedra"];
 
 // Loop para cada golpe, de 1 até a quantidade informada
-for (let i = 1; i <= quantidadeGolpes; i++) {
+for (let i = 0; i < quantidadeGolpes; i++) {
   // Calcula o índice do mineral usando o operador de módulo (%) para garantir que o índice esteja dentro do tamanho do array
   let minaIndex = i % minerais.length;
 
   // TODO: Agora exiba o índice i, concatene com o caractere ":", após, concatene com tipo de minerais[minaIndex]:
-  print();
+  print((i + 1) + ": " + minerais[minaIndex]);
 }
