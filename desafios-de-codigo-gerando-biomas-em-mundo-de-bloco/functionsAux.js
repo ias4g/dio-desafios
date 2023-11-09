8e4442e7f223bde0ec52837f74b41e0e722661af
Izael silva <izaell.oficial@gmail.com>
2023-11-09 17:34:17 -0300

    Update functionsAux.js

--- desafios-de-codigo-gerando-biomas-em-mundo-de-bloco/functionsAux.js
+++ desafios-de-codigo-gerando-biomas-em-mundo-de-bloco/functionsAux.js
@@ -1,10 +1,8 @@
-const entradas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+const numbers = [1, 2, 3, 4]
 function gets() {
-  const numeroAleatorio = Math.floor(Math.random() * 9);
-  const value = entradas[numeroAleatorio];
-
-  return value
+  const pos = Math.floor(Math.random() * numbers.length);
+  const retorno = numbers[pos]
+  return retorno
 }
 
 function print(texto) {
